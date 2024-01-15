99cde6edc444276bc4c2db29f7d2bcf9d3a45749
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-15 15:32:47 +0100

    added milestones

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('js ok');
+
+// ^ MILESTONE 1 - Creo un array di oggetti con le informazioni fornite 
+// ^ MILESTONE 2 - Stampo su console le informazioni del nome,ruolo e la foto
+// ^ MILESTONE 3 - Stampo le informazioni sul DOM
+
+
