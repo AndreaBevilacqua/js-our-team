0a971b79dcab3cc8e0ee317b523b651804faa497
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-15 15:47:11 +0100

    added objects array

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ console.log('js ok');
 // ^ MILESTONE 2 - Stampo su console le informazioni del nome,ruolo e la foto
 // ^ MILESTONE 3 - Stampo le informazioni sul DOM
 
+// ! ==================
+// ! RECUPERO ELEMENTI
+// ! ==================
+
+// ~ ==================
+// ~      ARRAY
+// ~ ==================
+
+const members = [
+    { name: 'Wayne Barnett', role: 'Founder & CEO', picture: 'wayne-barnett-founder-ceo.jpg'},
+    { name: 'Angela Caroll', role: 'Chief Editor', picture: 'angela-caroll-chief-editor.jpg'},
+    { name: 'Walter Gordon', role: 'Office manager', picture: 'walter-gordon-office-manager.jpg'},
+    { name: 'Angela Lopez', role: 'Social Media Manager', picture: 'angela-lopez-social-media-manager.jpg'},
+    { name: 'Scott Estrada', role: 'Developer', picture: 'scott-estrada-developer.jpg'},
+    { name: 'Barbara Ramos', role: 'Graphic Designer', picture: 'barbara-ramos-graphic-designer.jpg'}
+];
+
 
